8ca488570b3e8957b7c184018749cc676a672c24
Nathan Fleet <nfleet@kent.edu>
2023-04-12 21:04:04 -0400

    buy / sell fully functional

---
+++ src/components/SellTitle.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SellTitle = () => {
+  return (
+    <div className="bg-gray-100 py-12">
+      <div className="container mx-auto px-4">
+        <div className="max-w-lg mx-auto">
+          <h1 className="text-4xl font-bold text-center mb-4">Need Some New Kicks?</h1>
+          <p className="text-lg text-center">We offer the best selection!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SellTitle;
--- src/pages/Buy.jsx
+++ src/pages/Buy.jsx
@@ -1,33 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import SellTitle from '../components/SellTitle';
+import shoe from "../assets/shoe.png"
 
-function Buy() {
-  const [shoes, setShoes] = useState([]);
+const Buy = () => {
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
+    const fetchProducts = async () => {
       try {
-        const response = await axios.get('/api/shoes');
-        setShoes(response.data);
+        const response = await axios.get('http://localhost:4000/products');
+        if (response.data.data.length > 0) {
+          setProducts(response.data.data);
+        } else {
+          alert('No products found');
+        }
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
-    }
-    fetchData();
+    };
+
+    fetchProducts();
   }, []);
 
   return (
-    // needs updated
-    <div>
-      {shoes.map((shoe) => (
-        <div key={shoe.id}>
-          <h2>{shoe.name}</h2>
-          <p>{shoe.brand}</p>
-          <p>{shoe.price}</p>
+    <div className="bg-gray-100 h-screen">
+      <SellTitle />
+      <div className="flex flex-wrap justify-center items-center py-12">
+        {products.map((product) => (
+          <div key={product.ProductID} className="w-full md:w-1/2 lg:w-1/3 px-4 py-4 flex">
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden flex items-center justify-center w-1/3">
+            <img src={shoe} alt="Shoe" className="h-32 w-32" />
+          </div>
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden w-2/3">
+            <div className="px-4 py-2">
+              <h2 className="text-lg font-bold">{product.ProductName}</h2>
+              <p className="text-sm text-gray-600 font-bold">Brand:</p>
+              <p className="text-sm text-gray-600">{product.Brand}</p>
+              <p className="text-sm text-gray-600 font-bold">Model:</p>
+              <p className="text-sm text-gray-600">{product.Model}</p>
+              <p className="text-sm text-gray-600 font-bold">Quantity:</p>
+              <p className="text-sm text-gray-600">{product.Quantity}</p>
+              <p className="text-sm text-gray-600 font-bold">Price:</p>
+              <p className="text-sm text-gray-600">{product.Price}</p>
+              <p className="text-sm text-gray-600 font-bold">Color:</p>
+              <p className="text-sm text-gray-600">{product.Color}</p>
+            </div>
+            <div className="px-4 py-2 bg-white">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Buy Now
+              </button>
+            </div>
+          </div>
         </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Buy;
