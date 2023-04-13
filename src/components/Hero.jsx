8a0a4638cfeca4e64232331c16008018eff497b6
Nathan Fleet <nfleet@kent.edu>
2023-04-13 11:48:38 -0400

    hero section logo fix and animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import logo from '../assets/logo.png';
 
 function Hero() {
+  const rotationAnimation = {
+    rotate: {
+      rotate: [0, 360],
+      transition: { duration: 5, repeat: Infinity, ease: "linear" },
+    },
+  };
+
   return (
     <div className="bg-gray-900 text-white w-screen h-screen mt-5">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
         <div className="text-center">
-          <img src={logo} alt="logo" className="h-20 w-20 mx-auto" />
+          <div className="w-36 h-36 mx-auto">
+            <motion.img
+              src={logo}
+              alt="logo"
+              className="object-contain object-center h-full w-full"
+              initial="initial"
+              animate="rotate"
+              variants={rotationAnimation}
+            />
+          </div>
           <h1 className="mt-6 text-3xl font-extrabold sm:text-4xl">
             Welcome to SneakerSphere
           </h1>
